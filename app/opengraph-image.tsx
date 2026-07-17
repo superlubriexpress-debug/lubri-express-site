c25bb76bf61927619876e4e33a8ab127589e51ba
superlubriexpress-debug <superlubriexpress-debug@users.noreply.github.com>
2026-07-17 18:27:21 -0300

    Use deployment assets for OpenGraph image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -7,6 +7,8 @@ export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
 
 export default function OpenGraphImage() {
+  const assetBaseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : company.siteUrl;
+
   return new ImageResponse(
     (
       <div
@@ -23,7 +25,7 @@ export default function OpenGraphImage() {
       >
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src={`${company.siteUrl}/assets/real/fachada.jpg`}
+          src={`${assetBaseUrl}/assets/real/fachada.jpg`}
           alt=""
           width="1200"
           height="630"
@@ -40,7 +42,7 @@ export default function OpenGraphImage() {
         <div style={{ position: "relative", display: "flex", flexDirection: "column", justifyContent: "center", width: 700, padding: "64px 72px" }}>
           <div style={{ display: "flex", alignItems: "center", gap: 20 }}>
             {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src={`${company.siteUrl}/assets/brand/logo.png`} alt="" width="86" height="86" style={{ objectFit: "contain" }} />
+            <img src={`${assetBaseUrl}/assets/brand/logo.png`} alt="" width="86" height="86" style={{ objectFit: "contain" }} />
             <div style={{ display: "flex", flexDirection: "column" }}>
               <strong style={{ fontSize: 34, lineHeight: 1 }}>Lubri Express</strong>
               <span style={{ marginTop: 8, color: "#ffc400", fontSize: 18, fontWeight: 800, textTransform: "uppercase" }}>Auto Center</span>
